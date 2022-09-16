4dcf8f67e20079963c17e5d88984a5915f45cf6e
TalhaShakel <shakeeltalha637@gmail.com>
2022-09-15 23:14:20 -0700

    Project near to complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'package:weather_app/consts/images.dart';
+import 'package:weather_app/consts/strings.dart';
+import 'package:weather_app/controllers/main_controller.dart';
+import 'package:weather_app/models/current_weather_model.dart';
+import 'package:weather_app/models/hourly_weather_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,92 +32,300 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: WeatherApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var date = DateFormat("yMMMMd").format(DateTime.now());
+    var theme = Theme.of(context);
+    var controller = Get.put(MainController());
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        // backgroundColor: Vx,
+        appBar: AppBar(
+          title: date.text.color(theme.primaryColor).make(),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          actions: [
+            Obx(
+              () => IconButton(
+                  onPressed: () {
+                    controller.changeTheme();
+                  },
+                  icon: Icon(
+                      controller.isDark.value
+                          ? Icons.light_mode
+                          : Icons.dark_mode,
+                      color: theme.iconTheme.color)),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert, color: theme.iconTheme.color))
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Obx(
+          () => controller.isloaded.value == true
+              ? Container(
+                  padding: const EdgeInsets.all(12),
+                  child: FutureBuilder(
+                    future: controller.currentWeatherData,
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasData) {
+                        CurrentWeatherData data = snapshot.data;
+
+                        return SingleChildScrollView(
+                          physics: const BouncingScrollPhysics(),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              "${data.name}"
+                                  .text
+                                  .uppercase
+                                  .fontFamily("poppins_bold")
+                                  .size(32)
+                                  .letterSpacing(3)
+                                  .color(theme.primaryColor)
+                                  .make(),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Image.asset(
+                                    "assets/weather/${data.weather![0].icon}.png",
+                                    width: 80,
+                                    height: 80,
+                                  ),
+                                  RichText(
+                                      text: TextSpan(
+                                    children: [
+                                      TextSpan(
+                                          text: "${data.main!.temp}$degree",
+                                          style: TextStyle(
+                                            color: theme.primaryColor,
+                                            fontSize: 64,
+                                            fontFamily: "poppins",
+                                          )),
+                                      TextSpan(
+                                          text: " ${data.weather![0].main}",
+                                          style: TextStyle(
+                                            color: theme.primaryColor,
+                                            letterSpacing: 3,
+                                            fontSize: 14,
+                                            fontFamily: "poppins",
+                                          )),
+                                    ],
+                                  )),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  TextButton.icon(
+                                      onPressed: null,
+                                      icon: Icon(Icons.expand_less_rounded,
+                                          color: theme.iconTheme.color),
+                                      label: "${data.main!.tempMax}$degree"
+                                          .text
+                                          .color(theme.iconTheme.color)
+                                          .make()),
+                                  TextButton.icon(
+                                      onPressed: null,
+                                      icon: Icon(Icons.expand_more_rounded,
+                                          color: theme.iconTheme.color),
+                                      label: "${data.main!.tempMin}$degree"
+                                          .text
+                                          .color(theme.iconTheme.color)
+                                          .make())
+                                ],
+                              ),
+                              10.heightBox,
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: List.generate(3, (index) {
+                                  var iconsList = [clouds, humidity, windspeed];
+                                  var values = [
+                                    "${data.clouds!.all}",
+                                    "${data.main!.humidity}",
+                                    "${data.wind!.speed} km/h"
+                                  ];
+                                  return Column(
+                                    children: [
+                                      Image.asset(
+                                        iconsList[index],
+                                        width: 60,
+                                        height: 60,
+                                      )
+                                          .box
+                                          .gray200
+                                          .padding(const EdgeInsets.all(8))
+                                          .roundedSM
+                                          .make(),
+                                      10.heightBox,
+                                      values[index].text.gray400.make(),
+                                    ],
+                                  );
+                                }),
+                              ),
+                              10.heightBox,
+                              const Divider(),
+                              10.heightBox,
+                              FutureBuilder(
+                                future: controller.hourlyWeatherData,
+                                builder: (BuildContext context,
+                                    AsyncSnapshot snapshot) {
+                                  if (snapshot.hasData) {
+                                    HourlyWeatherData hourlyData =
+                                        snapshot.data;
+
+                                    return SizedBox(
+                                      height: 160,
+                                      child: ListView.builder(
+                                        physics: const BouncingScrollPhysics(),
+                                        scrollDirection: Axis.horizontal,
+                                        shrinkWrap: true,
+                                        itemCount: hourlyData.list!.length > 6
+                                            ? 6
+                                            : hourlyData.list!.length,
+                                        itemBuilder:
+                                            (BuildContext context, int index) {
+                                          var time = DateFormat.jm().format(
+                                              DateTime
+                                                  .fromMillisecondsSinceEpoch(
+                                                      hourlyData
+                                                              .list![index].dt!
+                                                              .toInt() *
+                                                          1000));
+
+                                          return Container(
+                                            padding: const EdgeInsets.all(8),
+                                            margin:
+                                                const EdgeInsets.only(right: 4),
+                                            decoration: BoxDecoration(
+                                              color: Vx.gray200,
+                                              borderRadius:
+                                                  BorderRadius.circular(12),
+                                            ),
+                                            child: Column(
+                                              children: [
+                                                time.text.make(),
+                                                Image.asset(
+                                                  "assets/weather/${hourlyData.list![index].weather![0].icon}.png",
+                                                  width: 80,
+                                                ),
+                                                10.heightBox,
+                                                "${hourlyData.list![index].main!.temp}$degree"
+                                                    .text
+                                                    .make(),
+                                              ],
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    );
+                                  }
+                                  return const Center(
+                                    child: CircularProgressIndicator(),
+                                  );
+                                },
+                              ),
+                              10.heightBox,
+                              const Divider(),
+                              10.heightBox,
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  "Next 7 Days"
+                                      .text
+                                      .semiBold
+                                      .size(16)
+                                      .color(theme.primaryColor)
+                                      .make(),
+                                  TextButton(
+                                      onPressed: () {},
+                                      child: "View All".text.make()),
+                                ],
+                              ),
+                              ListView.builder(
+                                physics: const NeverScrollableScrollPhysics(),
+                                shrinkWrap: true,
+                                itemCount: 7,
+                                itemBuilder: (BuildContext context, int index) {
+                                  var day = DateFormat("EEEE").format(
+                                      DateTime.now()
+                                          .add(Duration(days: index + 1)));
+                                  return Card(
+                                    child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 8, vertical: 12),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Expanded(
+                                              child: day.text.semiBold
+                                                  .color(theme.primaryColor)
+                                                  .make()),
+                                          Expanded(
+                                            child: TextButton.icon(
+                                                onPressed: null,
+                                                icon: Image.asset(
+                                                    "assets/weather/50n.png",
+                                                    width: 40),
+                                                label: "26$degree"
+                                                    .text
+                                                    .size(16)
+                                                    .color(theme.primaryColor)
+                                                    .make()),
+                                          ),
+                                          RichText(
+                                            text: TextSpan(
+                                              children: [
+                                                TextSpan(
+                                                    text: "37$degree /",
+                                                    style: TextStyle(
+                                                      color: theme.primaryColor,
+                                                      fontFamily: "poppins",
+                                                      fontSize: 16,
+                                                    )),
+                                                TextSpan(
+                                                    text: " 26$degree",
+                                                    style: TextStyle(
+                                                      color:
+                                                          theme.iconTheme.color,
+                                                      fontFamily: "poppins",
+                                                      fontSize: 16,
+                                                    )),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    },
+                  ),
+                )
+              : const Center(
+                  child: CircularProgressIndicator(),
+                ),
+        ));
   }
 }
